9c427c96ac38853bfc2f9d9c1afc2241a71d756c
BandeiraDenise <denise.bandeira@pucrs.br>
2024-07-30 00:41:14 -0300

    Add files via upload

---
+++ dados/init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Iniciar o banco de dados Oracle
+/etc/init.d/oracledb start
+
+# Esperar o banco de dados iniciar completamente
+sleep 60
+
+# Importar dados usando SQL*Loader
+sqlldr system/oracle@localhost:1521/XE control=/opt/oracle/scripts/TAB_AIR_AIRLINES.ctl
+
+# Manter o contêiner em execução
+tail -f /dev/null
