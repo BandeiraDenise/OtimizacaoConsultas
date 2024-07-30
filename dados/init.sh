dc738f97b9723ec2de45994552dbf2613e196a76
BandeiraDenise <denise.bandeira@pucrs.br>
2024-07-30 18:10:38 -0300

    Update init.sh

--- dados/init.sh
+++ dados/init.sh
@@ -11,10 +11,3 @@ tail -f /dev/null
 
 # Importar dados usando SQL*Loader
 sqlldr system/oracle@localhost:1521/XE control=/opt/oracle/scripts/TAB_AIR_AIRLINES.ctl
-
-# Verificar se a carga foi bem-sucedida
-if [ $? -eq 0 ]; then
-  echo "Dados carregados com sucesso!"
-else
-  echo "Erro ao carregar dados."
-fi
