36143928de342ed5b6d320129878a2b61515ee0a
BandeiraDenise <denise.bandeira@pucrs.br>
2024-07-30 17:48:31 -0300

    Update init.sh

--- dados/init.sh
+++ dados/init.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 # Iniciar o banco de dados Oracle
-/etc/init.d/oracledb start
+/etc/init.d/oracle-xe start
 
 # Esperar o banco de dados iniciar completamente
 sleep 60
 
-# Importar dados usando SQL*Loader
-#sqlldr system/oracle@localhost:1521/XE control=/opt/oracle/scripts/TAB_AIR_AIRLINES.ctl
-sqlldr system/oracle@localhost:1521/XE control=./OtimizacaoConsultas/dados/TAB_AIR_AIRLINES.ctl
-
 # Manter o contêiner em execução
 tail -f /dev/null
+
+# Importar dados usando SQL*Loader
+sqlldr system/oracle@localhost:1521/XE control=/opt/oracle/scripts/TAB_AIR_AIRLINES.ctl
+
+# Verificar se a carga foi bem-sucedida
+if [ $? -eq 0 ]; then
+  echo "Dados carregados com sucesso!"
+else
+  echo "Erro ao carregar dados."
+fi
